3cbcfc66e75d65bfb8fd201315f1656bdaa1cb03
phanlacphuc <phanlacphuc@gmail.com>
2015-11-12 16:22:52 +0900

    start-project

---
+++ location_identification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  location_identification
+//
+//  Created by Phan Lac Phuc on 11/12/15.
+//  Copyright © 2015 cmlab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
