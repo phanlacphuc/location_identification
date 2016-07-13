453424104b5bf10eb7f1d111a36481fe5ad60164
phanlacphuc <phanlacphuc@gmail.com>
2016-07-13 17:31:36 +0900

    catch ibeacon signal and send to server

--- location_identification/AppDelegate.swift
+++ location_identification/AppDelegate.swift
@@ -7,15 +7,65 @@
 //
 
 import UIKit
+import CoreLocation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    var locationManager: CLLocationManager?
+    var beaconRegion: CLBeaconRegion?
+    var beaconUUID: NSUUID?
+    var beaconIdentifier: String?
+    var apiURL: String?
+    var latitude: Double?
+    var longitude: Double?
+    var sessionNumber: Int? = 0
+    var testNumber: Int? = 0
+    
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        var myDict: NSDictionary?
+        if let path = NSBundle.mainBundle().pathForResource("Config", ofType: "plist") {
+            myDict = NSDictionary(contentsOfFile: path)
+        }
+        if let dict = myDict {
+            // Use your dict here
+            beaconUUID = NSUUID(UUIDString:(dict.valueForKey("Beacon UUID") as! String))
+            beaconIdentifier = (dict.valueForKey("Beacon Identifier") as! String)
+            apiURL = (dict.valueForKey("API URL") as! String)
+        }
+        
+        //Construct the region
+        beaconRegion = CLBeaconRegion(proximityUUID: beaconUUID!, identifier: beaconIdentifier!)
+        beaconRegion!.notifyEntryStateOnDisplay = true
+        
+        locationManager = CLLocationManager()
+        
+        
+        if(locationManager!.respondsToSelector("requestAlwaysAuthorization")) {
+            locationManager!.requestAlwaysAuthorization()
+        }
+        locationManager!.delegate = self
+        locationManager!.pausesLocationUpdatesAutomatically = false
+        
+        //the below 2 method can be called without dependence
+        locationManager!.startMonitoringForRegion(beaconRegion!)
+        
+        print("application launched")
+        if let options = launchOptions {
+            print("options")
+            // Do your checking on options here
+            let userInfo = launchOptions?[UIApplicationLaunchOptionsLocalNotificationKey] as! UILocalNotification!
+            if (userInfo != nil) {
+                print("userInfo.count > 0")
+            }
+        }
         return true
     }
 
@@ -40,7 +90,116 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
+    
+    func registerSession() {
+        
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: config)
+        
+        let url = NSURL(string: "server.php?registerSession", relativeToURL: NSURL(string: apiURL!))
+        
+        let req = NSURLRequest(URL: url!)
+        
+        print(req)
+        //NSURLSessionDownloadTask is retured from session.dataTaskWithRequest
+        let task = session.dataTaskWithRequest(req, completionHandler: {
+            (data, resp, err) in
+            print(resp!.URL!)
+            print(NSString(data: data!, encoding: NSUTF8StringEncoding))
+            
+            do {
+                let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions())
+                
+                if let sessionNumber = json["session_number"] as? Int {
+                    print("session number = \(sessionNumber)")
+                    self.sessionNumber = sessionNumber
+                    let defaults = NSUserDefaults.standardUserDefaults()
+                    defaults.setInteger(self.sessionNumber!, forKey: "session_number")
+                }
+            } catch {
+                print(error)
+            }
+        })
+        task.resume()
+    }
 
 }
 
+extension AppDelegate: CLLocationManagerDelegate {
+    func locationManager(manager: CLLocationManager, didStartMonitoringForRegion region: CLRegion) {
+        manager.requestStateForRegion(region)
+        print("didStartMonitoringForRegion \(region)")
+    }
+    
+    func locationManager(manager: CLLocationManager, monitoringDidFailForRegion region: CLRegion?, withError error: NSError) {
+        print("monitoringDidFailForRegion \(error)")
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("didFailWithError \(error)")
+    }
+    
+    func locationManager(manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], inRegion region: CLBeaconRegion) {
+        print("didRangeBeacons in region \(region)")
+        
+        dispatch_async(dispatch_get_main_queue(),{
+            let notificationName = "DidRangeBeacons"
+            let userInfo: NSDictionary = NSDictionary(object: beacons, forKey: "beacons")
+            NSNotificationCenter.defaultCenter().postNotificationName(notificationName, object: nil, userInfo: userInfo as [NSObject : AnyObject])
+        })
+        
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let tracking_flag = defaults.boolForKey("tracking_flag")
+        if !tracking_flag {
+            return
+        }
+        
+        for beacon in beacons {
+            if (beacon.rssi != 0 && sessionNumber != 0) {
+                let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+                let session = NSURLSession(configuration: config)
+                
+                let defaults = NSUserDefaults.standardUserDefaults()
+                testNumber = defaults.integerForKey("test_number")
+                
+                let paramString = "&ibeacon_major_number=\(beacon.major)&ibeacon_minor_number=\(beacon.minor)&latitude=\(latitude!)&longitude=\(longitude!)&rssi=\(beacon.rssi)&session_number=\(sessionNumber!)&test_number=\(testNumber!)"
+                
+                let url = NSURL(string: "server.php?postData\(paramString)", relativeToURL: NSURL(string: apiURL!))
+                
+                let req = NSURLRequest(URL: url!)
+                
+                //NSURLSessionDownloadTask is retured from session.dataTaskWithRequest
+                let task = session.dataTaskWithRequest(req, completionHandler: {
+                    (data, resp, err) in
+                    print(resp!.URL!)
+                    print(NSString(data: data!, encoding: NSUTF8StringEncoding))
+                })
+                task.resume()
+            }
+        }
+
+    }
+    
+    func locationManager(manager: CLLocationManager, didDetermineState state: CLRegionState, forRegion region: CLRegion) {
+        if (state == .Inside){
+            manager.startRangingBeaconsInRegion(region as! CLBeaconRegion)
+            manager.startUpdatingLocation()
+            print("did enter the region of \(region.identifier)")
+            
+            registerSession()
+            
+        }else{
+            manager.stopRangingBeaconsInRegion(region as! CLBeaconRegion)
+            manager.stopUpdatingLocation()
+            print("did exit the region of \(region.identifier)")
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        latitude = locValue.latitude
+        longitude = locValue.longitude
+    }
+}
--- location_identification/ViewController.swift
+++ location_identification/ViewController.swift
@@ -7,19 +7,166 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    private var realDistance : Int = 0
+    
+    private var nearbyBeacons: Array<CLBeacon> = []
+    
+    private var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        
+        let notificationName = "DidRangeBeacons"
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "handleReceivedDidRangeBeaconsNotification:", name: notificationName, object: nil)
+        
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    // MARK: - notification
+    
+    func handleReceivedDidRangeBeaconsNotification(notification: NSNotification) {
+        NSLog("handleReceivedDidRangeBeaconsNotification")
+        var userInfo : NSDictionary
+        userInfo = notification.userInfo!
+        
+        nearbyBeacons = userInfo.objectForKey("beacons") as! Array<CLBeacon>
+        self.tableView.reloadData()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 
 }
+extension ViewController{
+    
+    @IBAction func saveLogClicked() {
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        var tracking_flag = defaults.boolForKey("tracking_flag")
+        if (tracking_flag) == true {
+            tracking_flag = false
+            defaults.setBool(tracking_flag, forKey: "tracking_flag")
+            
+            let logMessage = "Tracking flag: \(tracking_flag)"
+            let alert: UIAlertController = UIAlertController(title: "Change Tracking Flag", message: logMessage, preferredStyle:  UIAlertControllerStyle.Alert)
+            
+            let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler:{
+                (action: UIAlertAction!) -> Void in
+                print("OK")
+            })
+            
+            
+            alert.addAction(defaultAction)
+            presentViewController(alert, animated: true, completion: nil)
+            
+        } else {
+            
+            var myDict: NSDictionary?
+            if let path = NSBundle.mainBundle().pathForResource("Config", ofType: "plist") {
+                myDict = NSDictionary(contentsOfFile: path)
+            }
+            var apiURL : String?
+            if let dict = myDict {
+                apiURL = (dict.valueForKey("API URL") as! String)
+                //apiURL = "http://192.168.2.150:8888/ObjectLocationTrackingApp/web/"
+            }
+            
+            
+            let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+            let session = NSURLSession(configuration: config)
+            
+            let url = NSURL(string: "server.php?registerTest", relativeToURL: NSURL(string: apiURL!))
+            
+            let req = NSURLRequest(URL: url!)
+            
+            print(req)
+            //NSURLSessionDownloadTask is retured from session.dataTaskWithRequest
+            let task = session.dataTaskWithRequest(req, completionHandler: {
+                (data, resp, err) in
+                print(resp!.URL!)
+                print(NSString(data: data!, encoding: NSUTF8StringEncoding))
+                
+                do {
+                    let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions())
+                    
+                    if let testNumber = json["test_number"] as? Int {
+                        print("test number = \(testNumber)")
+                        tracking_flag = true
+                        
+                        defaults.setBool(tracking_flag, forKey: "tracking_flag")
+                        defaults.setInteger(testNumber, forKey: "test_number")
+                        
+                        
+                        let logMessage = "Tracking flag: \(tracking_flag)"
+                        let alert: UIAlertController = UIAlertController(title: "Change Tracking Flag", message: logMessage, preferredStyle:  UIAlertControllerStyle.Alert)
+                        
+                        let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler:{
+                            (action: UIAlertAction!) -> Void in
+                            print("OK")
+                        })
+                        
+                        
+                        alert.addAction(defaultAction)
+                        self.presentViewController(alert, animated: true, completion: nil)
+                        
+                    }
+                } catch {
+                    print(error)
+                }
+            })
+            task.resume()
 
+        }
+        
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("Num: \(indexPath.row)")
+        print("Value: \(nearbyBeacons[indexPath.row])")
+        
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nearbyBeacons.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell:UITableViewCell? =
+        tableView.dequeueReusableCellWithIdentifier("MyCell") as UITableViewCell!
+        if(cell == nil) {
+            cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyCell")
+            cell!.selectionStyle = UITableViewCellSelectionStyle.None
+        }
+        
+        let beacon = nearbyBeacons[indexPath.row]
+        
+        let majorIdLabel = cell!.viewWithTag(1) as! UILabel
+        majorIdLabel.text = "Major : \(beacon.major.integerValue)"
+        let minorIdLabel = cell!.viewWithTag(2) as! UILabel
+        minorIdLabel.text = "Minor: \(beacon.minor.integerValue)"
+        let uuidLabel = cell!.viewWithTag(3)as! UILabel
+        uuidLabel.text = "UUID: \(beacon.proximityUUID.UUIDString)"
+        let rssiLabel = cell!.viewWithTag(4) as! UILabel
+        rssiLabel.text = "RSSI: \(beacon.rssi)"
+        
+        return cell!
+        
+    }
+    
+}
